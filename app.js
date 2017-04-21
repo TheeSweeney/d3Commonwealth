929799459c97b2dbc2693d21ddaf773e63d6f1bd
Brian Sweeney <bsweeney930@gmail.com>
2017-04-21 12:04:37 -0400

    reformatted spending data to get desired interactive behavior removed unnecessary state variable

--- app.js
+++ app.js
@@ -25,56 +25,69 @@ var qualityData = [
   {country: 'SWE', value: 1-0.82, rank: 11}
 ]
 var accessData = [
-{country: 'NETH', value:  1.7, rank: 1},
-{country: 'GER', value: 1.58, rank:  2},
-{country: 'UK', value:  1.39, rank:  3},
-{country: 'AUS', value: 1.19, rank:  4},
-{country: 'NZ', value:  1.14, rank:  5},
-{country: 'SWE', value: 1.06, rank:  6},
-{country: 'NOR', value: 1.02, rank:  7},
-{country: 'SWIZ', value:  1-0.11, rank: 8},
-{country: 'FRA', value: 1-0.15, rank: 9},
-{country: 'CAN', value: 1-0.77, rank: 10},
-{country: 'US', value:  1-1.07, rank: 11}
+  {country: 'NETH', value:  1.7, rank: 1},
+  {country: 'GER', value: 1.58, rank:  2},
+  {country: 'UK', value:  1.39, rank:  3},
+  {country: 'AUS', value: 1.19, rank:  4},
+  {country: 'NZ', value:  1.14, rank:  5},
+  {country: 'SWE', value: 1.06, rank:  6},
+  {country: 'NOR', value: 1.02, rank:  7},
+  {country: 'SWIZ', value:  1-0.11, rank: 8},
+  {country: 'FRA', value: 1-0.15, rank: 9},
+  {country: 'CAN', value: 1-0.77, rank: 10},
+  {country: 'US', value:  1-1.07, rank: 11}
 ]
 var adminData = [
-{country: 'AUS', value: 1.74, rank:  1},
-{country: 'NOR', value: 1.6, rank: 2},
-{country: 'UK', value:  1.59, rank:  3},
-{country: 'NZ', value:  1.54, rank:  4},
-{country: 'SWE', value: 1.26, rank:  5},
-{country: 'GER', value: 1.08, rank:  6},
-{country: 'CAN', value: 1.08, rank:  7},
-{country: 'SWIZ', value:  1-0.12, rank: 8},
-{country: 'NETH', value:  1-0.15, rank: 9},
-{country: 'US', value:  1-1.21, rank: 10},
-{country: 'FRA', value: 1-1.41, rank: 11}
+  {country: 'AUS', value: 2.74, rank:  1},
+  {country: 'NOR', value: 2.6, rank: 2},
+  {country: 'UK', value:  2.59, rank:  3},
+  {country: 'NZ', value:  2.54, rank:  4},
+  {country: 'SWE', value: 2.26, rank:  5},
+  {country: 'GER', value: 2.08, rank:  6},
+  {country: 'CAN', value: 2.08, rank:  7},
+  {country: 'SWIZ', value:  2-0.12, rank: 8},
+  {country: 'NETH', value:  2-0.15, rank: 9},
+  {country: 'US', value:  2-1.21, rank: 10},
+  {country: 'FRA', value: 2-1.41, rank: 11}
 ]
 var equityData = [
-{country: 'SWE', value: 1.55, rank:  1},
-{country: 'NZ', value:  1.39, rank:  2},
-{country: 'SWIZ', value:  1.32, rank:  3},
-{country: 'NETH', value:  1.06, rank:  4},
-{country: 'GER', value: 1.01, rank:  5},
-{country: 'NOR', value: 1-0.09, rank: 6},
-{country: 'AUS', value: 1-0.14, rank: 7},
-{country: 'CAN', value: 1-0.39, rank: 8},
-{country: 'FRA', value: 1-0.53, rank: 9},
-{country: 'UK', value:  1-0.63, rank: 10},
-{country: 'US', value:  1-0.76, rank: 11}
+  {country: 'SWE', value: 1.55, rank:  1},
+  {country: 'NZ', value:  1.39, rank:  2},
+  {country: 'SWIZ', value:  1.32, rank:  3},
+  {country: 'NETH', value:  1.06, rank:  4},
+  {country: 'GER', value: 1.01, rank:  5},
+  {country: 'NOR', value: 1-0.09, rank: 6},
+  {country: 'AUS', value: 1-0.14, rank: 7},
+  {country: 'CAN', value: 1-0.39, rank: 8},
+  {country: 'FRA', value: 1-0.53, rank: 9},
+  {country: 'UK', value:  1-0.63, rank: 10},
+  {country: 'US', value:  1-0.76, rank: 11}
 ]
 var outcomesData = [
-{country: 'AUS', value: 1.62, rank:  1},
-{country: 'SWE', value: 1.55, rank:  2},
-{country: 'NZ', value:  1.39, rank:  3},
-{country: 'SWIZ', value:  1.32, rank:  4},
-{country: 'FRA', value: 1.23, rank:  5},
-{country: 'NETH', value:  1.06, rank:  6},
-{country: 'NOR', value: 1-0.09, rank: 7},
-{country: 'GER', value: 1-0.18, rank: 8},
-{country: 'CAN', value: 1-0.39, rank: 9},
-{country: 'UK', value:  1-0.63, rank: 10},
-{country: 'US', value:  1-0.76, rank: 11}
+  {country: 'AUS', value: 1.62, rank:  1},
+  {country: 'SWE', value: 1.55, rank:  2},
+  {country: 'NZ', value:  1.39, rank:  3},
+  {country: 'SWIZ', value:  1.32, rank:  4},
+  {country: 'FRA', value: 1.23, rank:  5},
+  {country: 'NETH', value:  1.06, rank:  6},
+  {country: 'NOR', value: 1-0.09, rank: 7},
+  {country: 'GER', value: 1-0.18, rank: 8},
+  {country: 'CAN', value: 1-0.39, rank: 9},
+  {country: 'UK', value:  1-0.63, rank: 10},
+  {country: 'US', value:  1-0.76, rank: 11}
+]
+var spendingData = [
+  {country: 'NOR', value: 4038.00, rank:  1},
+  {country: 'UK', value:  4094.00, rank:  2},
+  {country: 'AUS', value: 4207.00, rank:  3},
+  {country: 'FRA', value: 4620.00, rank:  4},
+  {country: 'CAN', value: 4724.00, rank:  5},
+  {country: 'GER', value: 5119.00, rank:  6},
+  {country: 'NETH', value:  5277.00, rank:  7},
+  {country: 'SWE', value: 5306.00, rank:  8},
+  {country: 'NZ', value:  6432.00, rank:  9},
+  {country: 'SWIZ', value:  6787.00, rank:  10},
+  {country: 'US', value:  9364.00, rank:  11}
 ]
 
 var w = 800;
@@ -127,30 +140,28 @@ var yAxis = d3.svg.axis()
 var controls = d3.select('body')
                 .append('div')
                 .attr('id', 'controls');
+//TODO make button creation dryer
 var sort_overAll_btn = controls.append('button')
                 .html('Overall')
                 .classed('btn', true)
-                .attr('state', 'quality')
 var sort_quality_btn = controls.append('button')
                 .html('Quality')
                 .classed('btn', true)
-                .attr('state', 'quality')
 var sort_access_btn = controls.append('button')
                 .html('Access')
                 .classed('btn', true)
-                .attr('state', 'quality')
 var sort_admin_btn = controls.append('button')
-                .html('Administrative Efficiency')
+                .html('Admin. Efficiency')
                 .classed('btn', true)
-                .attr('state', 'quality')
 var sort_equity_btn = controls.append('button')
                 .html('Equity')
                 .classed('btn', true)
-                .attr('state', 'quality')
 var sort_outcomes_btn = controls.append('button')
                 .html('Health Outcomes')
                 .classed('btn', true)
-                .attr('state', 'quality')
+var sort_spending_btn = controls.append('button')
+                .html('Spending per Capita')
+                .classed('btn', true)
 function drawAxesAndLabels(params){
   if(params.initialize){
     this.append('g')//y axis
@@ -287,7 +298,7 @@ sort_overAll_btn.on('click', function(d){
   average: true
   })
 })
-
+//TODO function factory
 sort_quality_btn.on('click', function(d){
   plot.call(chart, {
     data: qualityData,
@@ -339,6 +350,18 @@ sort_equity_btn.on('click', function(d){
 sort_outcomes_btn.on('click', function(d){
   plot.call(chart, {
     data: outcomesData,
+    axis: {
+      x: xAxis,
+      y: yAxis
+    },
+  initialize: false,
+  average: false
+  })
+})
+
+sort_spending_btn.on('click', function(d){
+  plot.call(chart, {
+    data: spendingData,
     axis: {
       x: xAxis,
       y: yAxis
