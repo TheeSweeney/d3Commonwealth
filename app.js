bbc10fc8b42b32b820f59970a66f0e66401d999c
Brian Sweeney <bsweeney930@gmail.com>
2017-04-23 20:39:42 -0400

    more efficient storage of data

--- app.js
+++ app.js
@@ -1,98 +1,100 @@
 //BIG TODO - node.js refactor
 $(document).ready(function(){
 
-var data = [//TODO Rename this
-  {country: "UK", value: 1.367418713525976, rank: 1 },
-  {country: "AUS", value: 1.357430028143254, rank: 2 },
-  {country: "NETH", value: 1.251787757074009, rank: 3 },
-  {country: "NZ", value: 1.151665180612727, rank: 4 },
-  {country: "NOR", value: 1.13376085186227, rank: 5 },
-  {country: "SWIZ", value: 1.0675352522899197, rank: 6 },
-  {country: "SWE", value: 1.0674114551491452, rank: 7 },
-  {country: "GER", value: 1.064104826992644, rank: 8 },
-  {country: "CAN", value: 1-0.247553882107696, rank: 9 },
-  {country: "FRA", value: 1-0.41705654741814, rank: 10 },
-  {country: "USA", value: 1-0.706030660985181, rank: 11 }
-];
-var qualityData = [
-  {country: 'UK', value:  1.56, rank:  1},
-  {country: 'AUS', value: 1.38, rank:  2},
-  {country: 'NZ', value:  1.36, rank:  3},
-  {country: 'NETH', value:  1.29, rank:  4},
-  {country: 'US', value:  1.23, rank:  5},
-  {country: 'CAN', value: 1.15, rank:  6},
-  {country: 'SWIZ', value:  1-0.03, rank: 7},
-  {country: 'GER', value: 1-0.12, rank: 8},
-  {country: 'FRA', value: 1-0.42, rank: 9},
-  {country: 'NOR', value: 1-0.6, rank:  10},
-  {country: 'SWE', value: 1-0.82, rank: 11}
-]
-var accessData = [
-  {country: 'NETH', value:  1.7, rank: 1},
-  {country: 'GER', value: 1.58, rank:  2},
-  {country: 'UK', value:  1.39, rank:  3},
-  {country: 'AUS', value: 1.19, rank:  4},
-  {country: 'NZ', value:  1.14, rank:  5},
-  {country: 'SWE', value: 1.06, rank:  6},
-  {country: 'NOR', value: 1.02, rank:  7},
-  {country: 'SWIZ', value:  1-0.11, rank: 8},
-  {country: 'FRA', value: 1-0.15, rank: 9},
-  {country: 'CAN', value: 1-0.77, rank: 10},
-  {country: 'US', value:  1-1.07, rank: 11}
-]
-var adminData = [
-  {country: 'AUS', value: 2.74, rank:  1},
-  {country: 'NOR', value: 2.6, rank: 2},
-  {country: 'UK', value:  2.59, rank:  3},
-  {country: 'NZ', value:  2.54, rank:  4},
-  {country: 'SWE', value: 2.26, rank:  5},
-  {country: 'GER', value: 2.08, rank:  6},
-  {country: 'CAN', value: 2.08, rank:  7},
-  {country: 'SWIZ', value:  2-0.12, rank: 8},
-  {country: 'NETH', value:  2-0.15, rank: 9},
-  {country: 'US', value:  2-1.21, rank: 10},
-  {country: 'FRA', value: 2-1.41, rank: 11}
-]
-var equityData = [
-  {country: 'SWE', value: 1.55, rank:  1},
-  {country: 'NZ', value:  1.39, rank:  2},
-  {country: 'SWIZ', value:  1.32, rank:  3},
-  {country: 'NETH', value:  1.06, rank:  4},
-  {country: 'GER', value: 1.01, rank:  5},
-  {country: 'NOR', value: 1-0.09, rank: 6},
-  {country: 'AUS', value: 1-0.14, rank: 7},
-  {country: 'CAN', value: 1-0.39, rank: 8},
-  {country: 'FRA', value: 1-0.53, rank: 9},
-  {country: 'UK', value:  1-0.63, rank: 10},
-  {country: 'US', value:  1-0.76, rank: 11}
-]
-var outcomesData = [
-  {country: 'AUS', value: 1.62, rank:  1},
-  {country: 'SWE', value: 1.55, rank:  2},
-  {country: 'NZ', value:  1.39, rank:  3},
-  {country: 'SWIZ', value:  1.32, rank:  4},
-  {country: 'FRA', value: 1.23, rank:  5},
-  {country: 'NETH', value:  1.06, rank:  6},
-  {country: 'NOR', value: 1-0.09, rank: 7},
-  {country: 'GER', value: 1-0.18, rank: 8},
-  {country: 'CAN', value: 1-0.39, rank: 9},
-  {country: 'UK', value:  1-0.63, rank: 10},
-  {country: 'US', value:  1-0.76, rank: 11}
-]
-var spendingData = [
-  {country: 'NOR', value: 4038.00, rank:  1},
-  {country: 'UK', value:  4094.00, rank:  2},
-  {country: 'AUS', value: 4207.00, rank:  3},
-  {country: 'FRA', value: 4620.00, rank:  4},
-  {country: 'CAN', value: 4724.00, rank:  5},
-  {country: 'GER', value: 5119.00, rank:  6},
-  {country: 'NETH', value:  5277.00, rank:  7},
-  {country: 'SWE', value: 5306.00, rank:  8},
-  {country: 'NZ', value:  6432.00, rank:  9},
-  {country: 'SWIZ', value:  6787.00, rank:  10},
-  {country: 'US', value:  9364.00, rank:  11}
-]
-var currentDataSet = data;
+var dataSet =  {
+  dataOverall: [//TODO Rename this
+    {country: "UK", value: 1.367418713525976, rank: 1 },
+    {country: "AUS", value: 1.357430028143254, rank: 2 },
+    {country: "NETH", value: 1.251787757074009, rank: 3 },
+    {country: "NZ", value: 1.151665180612727, rank: 4 },
+    {country: "NOR", value: 1.13376085186227, rank: 5 },
+    {country: "SWIZ", value: 1.0675352522899197, rank: 6 },
+    {country: "SWE", value: 1.0674114551491452, rank: 7 },
+    {country: "GER", value: 1.064104826992644, rank: 8 },
+    {country: "CAN", value: 1-0.247553882107696, rank: 9 },
+    {country: "FRA", value: 1-0.41705654741814, rank: 10 },
+    {country: "USA", value: 1-0.706030660985181, rank: 11 }
+  ],
+  qualityData: [
+    {country: 'UK', value:  1.56, rank:  1},
+    {country: 'AUS', value: 1.38, rank:  2},
+    {country: 'NZ', value:  1.36, rank:  3},
+    {country: 'NETH', value:  1.29, rank:  4},
+    {country: 'US', value:  1.23, rank:  5},
+    {country: 'CAN', value: 1.15, rank:  6},
+    {country: 'SWIZ', value:  1-0.03, rank: 7},
+    {country: 'GER', value: 1-0.12, rank: 8},
+    {country: 'FRA', value: 1-0.42, rank: 9},
+    {country: 'NOR', value: 1-0.6, rank:  10},
+    {country: 'SWE', value: 1-0.82, rank: 11}
+  ],
+  accessData: [
+    {country: 'NETH', value:  1.7, rank: 1},
+    {country: 'GER', value: 1.58, rank:  2},
+    {country: 'UK', value:  1.39, rank:  3},
+    {country: 'AUS', value: 1.19, rank:  4},
+    {country: 'NZ', value:  1.14, rank:  5},
+    {country: 'SWE', value: 1.06, rank:  6},
+    {country: 'NOR', value: 1.02, rank:  7},
+    {country: 'SWIZ', value:  1-0.11, rank: 8},
+    {country: 'FRA', value: 1-0.15, rank: 9},
+    {country: 'CAN', value: 1-0.77, rank: 10},
+    {country: 'US', value:  1-1.07, rank: 11}
+  ],
+  adminData: [
+    {country: 'AUS', value: 2.74, rank:  1},
+    {country: 'NOR', value: 2.6, rank: 2},
+    {country: 'UK', value:  2.59, rank:  3},
+    {country: 'NZ', value:  2.54, rank:  4},
+    {country: 'SWE', value: 2.26, rank:  5},
+    {country: 'GER', value: 2.08, rank:  6},
+    {country: 'CAN', value: 2.08, rank:  7},
+    {country: 'SWIZ', value:  2-0.12, rank: 8},
+    {country: 'NETH', value:  2-0.15, rank: 9},
+    {country: 'US', value:  2-1.21, rank: 10},
+    {country: 'FRA', value: 2-1.41, rank: 11}
+  ],
+  equityData: [
+    {country: 'SWE', value: 1.55, rank:  1},
+    {country: 'NZ', value:  1.39, rank:  2},
+    {country: 'SWIZ', value:  1.32, rank:  3},
+    {country: 'NETH', value:  1.06, rank:  4},
+    {country: 'GER', value: 1.01, rank:  5},
+    {country: 'NOR', value: 1-0.09, rank: 6},
+    {country: 'AUS', value: 1-0.14, rank: 7},
+    {country: 'CAN', value: 1-0.39, rank: 8},
+    {country: 'FRA', value: 1-0.53, rank: 9},
+    {country: 'UK', value:  1-0.63, rank: 10},
+    {country: 'US', value:  1-0.76, rank: 11}
+  ],
+  outcomesData: [
+    {country: 'AUS', value: 1.62, rank:  1},
+    {country: 'SWE', value: 1.55, rank:  2},
+    {country: 'NZ', value:  1.39, rank:  3},
+    {country: 'SWIZ', value:  1.32, rank:  4},
+    {country: 'FRA', value: 1.23, rank:  5},
+    {country: 'NETH', value:  1.06, rank:  6},
+    {country: 'NOR', value: 1-0.09, rank: 7},
+    {country: 'GER', value: 1-0.18, rank: 8},
+    {country: 'CAN', value: 1-0.39, rank: 9},
+    {country: 'UK', value:  1-0.63, rank: 10},
+    {country: 'US', value:  1-0.76, rank: 11}
+  ],
+  spendingData: [
+    {country: 'NOR', value: 4038.00, rank:  1},
+    {country: 'UK', value:  4094.00, rank:  2},
+    {country: 'AUS', value: 4207.00, rank:  3},
+    {country: 'FRA', value: 4620.00, rank:  4},
+    {country: 'CAN', value: 4724.00, rank:  5},
+    {country: 'GER', value: 5119.00, rank:  6},
+    {country: 'NETH', value:  5277.00, rank:  7},
+    {country: 'SWE', value: 5306.00, rank:  8},
+    {country: 'NZ', value:  6432.00, rank:  9},
+    {country: 'SWIZ', value:  6787.00, rank:  10},
+    {country: 'US', value:  9364.00, rank:  11}
+  ]
+}
+var currentDataSet = dataSet.dataOverall;
 var avgShow = true;
 var w = window.outerWidth - 6;
 var h = .5625 * w;
@@ -117,22 +119,22 @@ var chart = svg.append("g")
       .classed("display", true)
       .attr("transform", "translate(" + margin.left + "," + (margin.top + 50) + ")");
 var x = d3.scale.linear()
-          .domain(d3.extent(data, function(d){
+          .domain(d3.extent(dataSet.dataOverall, function(d){
             return d.rank;
           }))
           .range([10, width]);
 var xPoints = d3.scale.linear()
-          .domain(d3.extent(data, function(d){
+          .domain(d3.extent(dataSet.dataOverall, function(d){
             return d.rank;
           }))
           .range([50, width]);
 var y = d3.scale.linear()
-          .domain([0, d3.max(data, function(d){
+          .domain([0, d3.max(dataSet.dataOverall, function(d){
             return d.value + .1;
           })])
           .range([height, 0])
 var linearColorScale = d3.scale.linear()
-                        .domain([0, data.length])
+                        .domain([0, dataSet.dataOverall.length])
                         .range(['#4ABDBC','#044C7F']);
 var xAxis = d3.svg.axis()
               .scale(x)
@@ -304,7 +306,7 @@ function plot(params){
 
 sort_overAll_btn.on('click', function(d){
   avgShow = true;//TODO there is a more efficient way to organize these variables
-  currentDataSet = data;
+  currentDataSet = dataSet.dataOverall;
   plot.call(chart, {
     data: currentDataSet,
     axis: {
@@ -318,7 +320,7 @@ sort_overAll_btn.on('click', function(d){
 //TODO function factory
 sort_quality_btn.on('click', function(d){
   avgShow = false;
-  currentDataSet = qualityData;
+  currentDataSet = dataSet.qualityData;
   plot.call(chart, {
     data: currentDataSet,
     axis: {
@@ -332,7 +334,7 @@ sort_quality_btn.on('click', function(d){
 
 sort_access_btn.on('click', function(d){
   avgShow = false;
-  currentDataSet = accessData;
+  currentDataSet = dataSet.accessData;
   plot.call(chart, {
     data: currentDataSet,
     axis: {
@@ -346,7 +348,7 @@ sort_access_btn.on('click', function(d){
 
 sort_admin_btn.on('click', function(d){
   avgShow = false;
-  currentDataSet = adminData;
+  currentDataSet = dataSet.adminData;
   plot.call(chart, {
     data: currentDataSet,
     axis: {
@@ -360,7 +362,7 @@ sort_admin_btn.on('click', function(d){
 
 sort_equity_btn.on('click', function(d){
   avgShow = false;
-  currentDataSet = equityData;
+  currentDataSet = dataSet.equityData;
   plot.call(chart, {
     data: currentDataSet,
     axis: {
@@ -374,7 +376,7 @@ sort_equity_btn.on('click', function(d){
 
 sort_outcomes_btn.on('click', function(d){
   avgShow = false;
-  currentDataSet = outcomesData;
+  currentDataSet = dataSet.outcomesData;
   plot.call(chart, {
     data: currentDataSet,
     axis: {
@@ -388,7 +390,7 @@ sort_outcomes_btn.on('click', function(d){
 
 sort_spending_btn.on('click', function(d){
   avgShow = false;
-  currentDataSet = spendingData;
+  currentDataSet = dataSet.spendingData;
   plot.call(chart, {
     data: currentDataSet,
     axis: {
