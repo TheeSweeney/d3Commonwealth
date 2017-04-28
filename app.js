6a8f95f781abd8543aad55dc22d8ff80b0d09ee3
Brian Sweeney <bsweeney930@gmail.com>
2017-04-28 02:37:11 -0400

    removed unnecessary styling placed data in its own file

--- app.js
+++ app.js
@@ -1,106 +1,6 @@
 //BIG TODO - node.js refactor
 $(document).ready(function(){
 
-var elevenCountries = ' Score in Eleven Countries'
-
-var dataSet =  {
-  dataOverall:{
-    data: [//TODO Rename this
-      {country: "UK", value: 1.367418713525976, rank: 1 },
-      {country: "AUS", value: 1.357430028143254, rank: 2 },
-      {country: "NETH", value: 1.251787757074009, rank: 3 },
-      {country: "NZ", value: 1.151665180612727, rank: 4 },
-      {country: "NOR", value: 1.13376085186227, rank: 5 },
-      {country: "SWIZ", value: 1.0675352522899197, rank: 6 },
-      {country: "SWE", value: 1.0674114551491452, rank: 7 },
-      {country: "GER", value: 1.064104826992644, rank: 8 },
-      {country: "CAN", value: 1-0.247553882107696, rank: 9 },
-      {country: "FRA", value: 1-0.41705654741814, rank: 10 },
-      {country: "US", value: 1-0.706030660985181, rank: 11 }
-    ],
-    title: 'Health System Performance' + elevenCountries
-  },
-  qualityData:{
-    data: [
-      {country: 'UK', value:  1.56, rank:  1},
-      {country: 'AUS', value: 1.38, rank:  2},
-      {country: 'NETH', value:  1.29, rank:  4},
-      {country: 'NZ', value:  1.36, rank:  3},
-      {country: 'NOR', value: 1-0.6, rank:  10},
-      {country: 'SWIZ', value:  1-0.03, rank: 7},
-      {country: 'SWE', value: 1-0.82, rank: 11},
-      {country: 'GER', value: 1-0.12, rank: 8},
-      {country: 'CAN', value: 1.15, rank:  6},
-      {country: 'FRA', value: 1-0.42, rank: 9},
-      {country: 'US', value:  1.23, rank:  5}
-    ],
-    title: 'Quality of Healthcare' + elevenCountries
-  },
-  accessData:{
-    data: [
-      {country: 'UK', value:  1.39, rank:  3},
-      {country: 'AUS', value: 1.19, rank:  4},
-      {country: 'NETH', value:  1.7, rank: 1},
-      {country: 'NZ', value:  1.14, rank:  5},
-      {country: 'NOR', value: 1.02, rank:  7},
-      {country: 'SWIZ', value:  1-0.11, rank: 8},
-      {country: 'SWE', value: 1.06, rank:  6},
-      {country: 'GER', value: 1.58, rank:  2},
-      {country: 'CAN', value: 1-0.77, rank: 10},
-      {country: 'FRA', value: 1-0.15, rank: 9},
-      {country: 'US', value:  1-1.07, rank: 11}
-    ],
-    title: 'Access to Healthcare' + elevenCountries
-  },
-  adminData:{
-    data: [
-      {country: 'UK', value:  2.59, rank:  3},
-      {country: 'AUS', value: 2.74, rank:  1},
-      {country: 'NETH', value:  2-0.15, rank: 9},
-      {country: 'NZ', value:  2.54, rank:  4},
-      {country: 'NOR', value: 2.6, rank: 2},
-      {country: 'SWIZ', value:  2-0.12, rank: 8},
-      {country: 'SWE', value: 2.26, rank:  5},
-      {country: 'GER', value: 2.08, rank:  6},
-      {country: 'CAN', value: 2.08, rank:  7},
-      {country: 'US', value:  2-1.21, rank: 10},
-      {country: 'FRA', value: 2-1.41, rank: 11}
-    ],
-    title: 'Administrative Efficiency' + elevenCountries
-  },
-  equityData:{
-    data: [
-      {country: 'UK', value:  1-0.63, rank: 10},
-      {country: 'AUS', value: 1-0.14, rank: 7},
-      {country: 'NETH', value:  1.06, rank:  4},
-      {country: 'NZ', value:  1.39, rank:  2},
-      {country: 'NOR', value: 1-0.09, rank: 6},
-      {country: 'SWIZ', value:  1.32, rank:  3},
-      {country: 'SWE', value: 1.55, rank:  1},
-      {country: 'GER', value: 1.01, rank:  5},
-      {country: 'CAN', value: 1-0.39, rank: 8},
-      {country: 'FRA', value: 1-0.53, rank: 9},
-      {country: 'US', value:  1-0.76, rank: 11}
-    ],
-    title: 'Equity' + elevenCountries
-  },
-  outcomesData:{
-    data: [
-      {country: 'UK', value:  1-0.63, rank: 10},
-      {country: 'AUS', value: 1.62, rank:  1},
-      {country: 'NETH', value:  1.06, rank:  6},
-      {country: 'NZ', value:  1.39, rank:  3},
-      {country: 'NOR', value: 1-0.09, rank: 7},
-      {country: 'SWIZ', value:  1.32, rank:  4},
-      {country: 'SWE', value: 1.55, rank:  2},
-      {country: 'GER', value: 1-0.18, rank: 8},
-      {country: 'CAN', value: 1-0.39, rank: 9},
-      {country: 'FRA', value: 1.23, rank:  5},
-      {country: 'US', value:  1-0.76, rank: 11}
-    ],
-    title: 'Healthcare Outcomes' + elevenCountries
-  }
-}
 var currentDataSet = dataSet.dataOverall.data;
 var currentTitle = dataSet.dataOverall.title
 var w = window.outerWidth - 6;
@@ -203,7 +103,7 @@ var sort_access_btn = controls.append('button')
                 .attr('id', 'accessBtn')
                 .classed('btn', true)
 var sort_admin_btn = controls.append('button')
-                .html('Admin. Efficiency')
+                .html('Administrative Efficiency')
                 .attr('id', 'adminBtn')
                 .classed('btn', true)
 var sort_equity_btn = controls.append('button')
@@ -221,7 +121,7 @@ function drawAxesAndLabels(params){
     bottom: 'Lower Performing'
   }
 
-  function yAxesAndLabels() {
+  function yAxesAndLabels() {//TODO factor out to prevent code repition in this and exhibit 5
     this.append('g')//y axis
         .classed('y axis grad', true)
         .attr('transform', 'translate(0,0)')
@@ -260,13 +160,14 @@ function drawAxesAndLabels(params){
         })
         .attr('transform', 'translate(-35,' + (height - 30) + ')')
         .style('fill', '#044C7F')
-        
-        
-
-
   }
 
 
+  if(!params.initialize){//replot y axis on resize
+    this.select('.y.axis')
+        .remove()
+    yAxesAndLabels.call(this)
+  }
 
   if(params.initialize){
     svg.insert('text')
---
+++ data.js
@@ -0,0 +1,100 @@
+var elevenCountries = ' Score in Eleven Countries'
+
+var dataSet =  {
+  dataOverall:{
+    data: [//TODO Rename this
+      {country: "UK", value: 1.367418713525976, rank: 1 },
+      {country: "AUS", value: 1.357430028143254, rank: 2 },
+      {country: "NETH", value: 1.251787757074009, rank: 3 },
+      {country: "NZ", value: 1.151665180612727, rank: 4 },
+      {country: "NOR", value: 1.13376085186227, rank: 5 },
+      {country: "SWIZ", value: 1.0675352522899197, rank: 6 },
+      {country: "SWE", value: 1.0674114551491452, rank: 7 },
+      {country: "GER", value: 1.064104826992644, rank: 8 },
+      {country: "CAN", value: 1-0.247553882107696, rank: 9 },
+      {country: "FRA", value: 1-0.41705654741814, rank: 10 },
+      {country: "US", value: 1-0.706030660985181, rank: 11 }
+    ],
+    title: 'Health System Performance' + elevenCountries
+  },
+  qualityData:{
+    data: [
+      {country: 'UK', value:  1.56, rank:  1},
+      {country: 'AUS', value: 1.38, rank:  2},
+      {country: 'NETH', value:  1.29, rank:  4},
+      {country: 'NZ', value:  1.36, rank:  3},
+      {country: 'NOR', value: 1-0.6, rank:  10},
+      {country: 'SWIZ', value:  1-0.03, rank: 7},
+      {country: 'SWE', value: 1-0.82, rank: 11},
+      {country: 'GER', value: 1-0.12, rank: 8},
+      {country: 'CAN', value: 1.15, rank:  6},
+      {country: 'FRA', value: 1-0.42, rank: 9},
+      {country: 'US', value:  1.23, rank:  5}
+    ],
+    title: 'Quality of Healthcare' + elevenCountries
+  },
+  accessData:{
+    data: [
+      {country: 'UK', value:  1.39, rank:  3},
+      {country: 'AUS', value: 1.19, rank:  4},
+      {country: 'NETH', value:  1.7, rank: 1},
+      {country: 'NZ', value:  1.14, rank:  5},
+      {country: 'NOR', value: 1.02, rank:  7},
+      {country: 'SWIZ', value:  1-0.11, rank: 8},
+      {country: 'SWE', value: 1.06, rank:  6},
+      {country: 'GER', value: 1.58, rank:  2},
+      {country: 'CAN', value: 1-0.77, rank: 10},
+      {country: 'FRA', value: 1-0.15, rank: 9},
+      {country: 'US', value:  1-1.07, rank: 11}
+    ],
+    title: 'Access to Healthcare' + elevenCountries
+  },
+  adminData:{
+    data: [
+      {country: 'UK', value:  2.59, rank:  3},
+      {country: 'AUS', value: 2.74, rank:  1},
+      {country: 'NETH', value:  2-0.15, rank: 9},
+      {country: 'NZ', value:  2.54, rank:  4},
+      {country: 'NOR', value: 2.6, rank: 2},
+      {country: 'SWIZ', value:  2-0.12, rank: 8},
+      {country: 'SWE', value: 2.26, rank:  5},
+      {country: 'GER', value: 2.08, rank:  6},
+      {country: 'CAN', value: 2.08, rank:  7},
+      {country: 'US', value:  2-1.21, rank: 10},
+      {country: 'FRA', value: 2-1.41, rank: 11}
+    ],
+    title: 'Administrative Efficiency' + elevenCountries
+  },
+  equityData:{
+    data: [
+      {country: 'UK', value:  1-0.63, rank: 10},
+      {country: 'AUS', value: 1-0.14, rank: 7},
+      {country: 'NETH', value:  1.06, rank:  4},
+      {country: 'NZ', value:  1.39, rank:  2},
+      {country: 'NOR', value: 1-0.09, rank: 6},
+      {country: 'SWIZ', value:  1.32, rank:  3},
+      {country: 'SWE', value: 1.55, rank:  1},
+      {country: 'GER', value: 1.01, rank:  5},
+      {country: 'CAN', value: 1-0.39, rank: 8},
+      {country: 'FRA', value: 1-0.53, rank: 9},
+      {country: 'US', value:  1-0.76, rank: 11}
+    ],
+    title: 'Equity' + elevenCountries
+  },
+  outcomesData:{
+    data: [
+      {country: 'UK', value:  1-0.63, rank: 10},
+      {country: 'AUS', value: 1.62, rank:  1},
+      {country: 'NETH', value:  1.06, rank:  6},
+      {country: 'NZ', value:  1.39, rank:  3},
+      {country: 'NOR', value: 1-0.09, rank: 7},
+      {country: 'SWIZ', value:  1.32, rank:  4},
+      {country: 'SWE', value: 1.55, rank:  2},
+      {country: 'GER', value: 1-0.18, rank: 8},
+      {country: 'CAN', value: 1-0.39, rank: 9},
+      {country: 'FRA', value: 1.23, rank:  5},
+      {country: 'US', value:  1-0.76, rank: 11}
+    ],
+    title: 'Healthcare Outcomes' + elevenCountries
+  }
+}
